0c5219505334b9cbb5437cb71b6a8bb0c2bfb2d1
Christina Cornell <cdhorn515@gmail.com>
2018-03-16 12:33:40 -0400

    starting to learn some C#, using Tabor tutorial and experimenting

---
+++ TaborTutorial/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TaborTutorial
+{
+    public class Hello
+    {
+        public void sayHello() {
+            Console.WriteLine("What's your name again?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello from the hello class, {name}!");
+        }
+
+    }
+}
---
+++ TaborTutorial/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace TaborTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+         
+            Console.WriteLine("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+            //or int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 21) {
+                Console.WriteLine($"Hello {name}, you are {age} years old, would you like a drink?");
+            } else {
+                Console.WriteLine($"Hello {name}, you are {age} years old, would you like some coffee?");
+            }
+
+            int[] numbers = new int[5];
+
+            numbers[0] = 1;
+            numbers[1] = 5;
+            numbers[2] = 34;
+            numbers[3] = 72;
+            numbers[4] = 0;
+
+
+            //Console.WriteLine(numbers[3]);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            // can also loop using foreach:
+            foreach (int number in numbers)
+            {
+                Console.WriteLine($"Number in numbers array {number}.");
+            }
+            Console.WriteLine("Pick a number between 1 and 5: ");
+            int pickANumber = Convert.ToInt32(Console.ReadLine());
+
+            switch(pickANumber){
+                case 1:
+                    Console.WriteLine($"You picked the first number, {numbers[0]}");
+                    break;
+                case 2:
+                    Console.WriteLine($"You picked the second number, {numbers[1]}");
+                    break;
+                case 3:
+                    Console.WriteLine($"You picked the third number, {numbers[2]}");
+                    break;
+                case 4:
+                    Console.WriteLine($"You picked the fourth number, {numbers[3]}");
+                    break;
+                case 5:
+                    Console.WriteLine($"You picked the fifth number, {numbers[4]}");
+                    break;
+                default:
+                    Console.WriteLine("Read the directions and try again some other time!");
+                    break;
+            }
+
+            char[] nameArray = name.ToCharArray();
+            //Array.Reverse(nameArray);
+            //foreach (char letter in nameArray)
+            //{
+            //    Console.Write(letter);
+            //}
+            Console.Write("Your name backwards is: ");
+            for (int i = nameArray.Length - 1; i >= 0; i--) {
+                Console.Write(nameArray[i]);
+            }
+            Console.WriteLine();
+
+            //sayHello(name);
+            //Hello hi = new Hello();
+            //var instance = new Hello();
+            //instance.sayHello();
+            Hello hi = new Hello();
+            hi.sayHello();
+
+        }
+
+        //static void sayHello(string name) {
+        //    Console.WriteLine($"Hello there {name}.");
+        //}
+
+
+    }
+}
